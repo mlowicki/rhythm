2ee0f7c9696542f70f85d9df91da7d6346b1086d
Michał Łowicki <mlowicki@gmail.com>
2018-08-20 12:49:29 +0200

    Refactor api.go after moving to separate package

--- api/api.go
+++ api/api.go
@@ -2,7 +2,7 @@ package api
 
 import (
 	"encoding/json"
-	"fmt"
+	"errors"
 	"log"
 	"net/http"
 	"time"
@@ -14,87 +14,88 @@ import (
 )
 
 var (
-	apiErrAccessForbidden = fmt.Errorf("Access forbidden")
-	apiErrUnauthorized    = fmt.Errorf("Unauthorized")
+	errForbidden    = errors.New("Forbidden")
+	errUnauthorized = errors.New("Unauthorized")
 )
 
-type Authorizer interface {
+type authorizer interface {
 	GetProjectAccessLevel(r *http.Request, group string, project string) (auth.AccessLevel, error)
 }
 
-type apiHandler struct {
-	A Authorizer
-	S Storage
-	H func(auth Authorizer, s Storage, w http.ResponseWriter, r *http.Request) error
+type storage interface {
+	GetJobs() ([]*model.Job, error)
+	GetGroupJobs(group string) ([]*model.Job, error)
+	GetProjectJobs(group string, project string) ([]*model.Job, error)
+	GetJob(group string, project string, id string) (*model.Job, error)
+	SaveJob(j *model.Job) error
+	DeleteJob(group string, project string, id string) error
 }
 
-func (h *apiHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	err := h.H(h.A, h.S, w, r)
+type handler struct {
+	a authorizer
+	s storage
+	h func(auth authorizer, s storage, w http.ResponseWriter, r *http.Request) error
+}
+
+func (h *handler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	err := h.h(h.a, h.s, w, r)
 	if err != nil {
-		switch e := err.(type) {
-		case *apiErr:
-			w.WriteHeader(e.Status)
-			err = json.NewEncoder(w).Encode(e)
-			if err != nil {
-				log.Fatal(err)
-			}
-		default:
-			log.Fatal(err)
-		}
+		json.NewEncoder(w).Encode(errResp{err.Error()})
 	}
 }
 
-type apiErr struct {
-	Message string `json:"error"`
-	Status  int    `json:"-"`
+type errResp struct {
+	Error string `json:"error"`
 }
 
-func (e *apiErr) Error() string {
-	return e.Message
-}
-
-func getJobs(_ Authorizer, s Storage, w http.ResponseWriter, r *http.Request) error {
+func getJobs(_ authorizer, s storage, w http.ResponseWriter, r *http.Request) error {
 	jobs, err := s.GetJobs()
 	if err != nil {
-		return &apiErr{err.Error(), http.StatusInternalServerError}
+		w.WriteHeader(http.StatusInternalServerError)
+		return err
 	}
 	json.NewEncoder(w).Encode(jobs)
 	return nil
 }
 
-func getGroupJobs(_ Authorizer, s Storage, w http.ResponseWriter, r *http.Request) error {
+func getGroupJobs(_ authorizer, s storage, w http.ResponseWriter, r *http.Request) error {
 	jobs, err := s.GetGroupJobs(mux.Vars(r)["group"])
 	if err != nil {
-		return &apiErr{err.Error(), http.StatusInternalServerError}
+		w.WriteHeader(http.StatusInternalServerError)
+		return err
 	}
 	json.NewEncoder(w).Encode(jobs)
 	return nil
 }
 
-func getProjectJobs(_ Authorizer, s Storage, w http.ResponseWriter, r *http.Request) error {
+func getProjectJobs(_ authorizer, s storage, w http.ResponseWriter, r *http.Request) error {
 	vars := mux.Vars(r)
 	jobs, err := s.GetProjectJobs(vars["group"], vars["project"])
 	if err != nil {
-		return &apiErr{err.Error(), http.StatusInternalServerError}
+		w.WriteHeader(http.StatusInternalServerError)
+		return err
 	}
 	json.NewEncoder(w).Encode(jobs)
 	return nil
 }
 
-func getJob(a Authorizer, s Storage, w http.ResponseWriter, r *http.Request) error {
+func getJob(a authorizer, s storage, w http.ResponseWriter, r *http.Request) error {
 	vars := mux.Vars(r)
 	group := vars["group"]
 	project := vars["project"]
 	accessLevel, err := a.GetProjectAccessLevel(r, group, project)
 	if err != nil {
-		return &apiErr{err.Error(), http.StatusInternalServerError}
+		w.WriteHeader(http.StatusInternalServerError)
+		return err
 	}
 	if accessLevel == auth.NoAccess {
-		return &apiErr{apiErrAccessForbidden.Error(), http.StatusForbidden}
+		w.WriteHeader(http.StatusForbidden)
+		return errForbidden
 	}
 	job, err := s.GetJob(group, project, vars["id"])
 	if err != nil {
-		return &apiErr{err.Error(), http.StatusInternalServerError}
+		w.WriteHeader(http.StatusInternalServerError)
+		return err
 	}
 	if job == nil {
 		w.WriteHeader(http.StatusNotFound)
@@ -104,23 +105,26 @@ func getJob(a Authorizer, s Storage, w http.ResponseWriter, r *http.Request) err
 	return nil
 }
 
-func deleteJob(a Authorizer, s Storage, w http.ResponseWriter, r *http.Request) error {
+func deleteJob(a authorizer, s storage, w http.ResponseWriter, r *http.Request) error {
 	vars := mux.Vars(r)
 	accessLevel, err := a.GetProjectAccessLevel(r, vars["group"], vars["project"])
 	if err != nil {
-		return &apiErr{err.Error(), http.StatusInternalServerError}
+		w.WriteHeader(http.StatusInternalServerError)
+		return err
 	}
 	if accessLevel != auth.ReadWrite {
-		return &apiErr{apiErrAccessForbidden.Error(), http.StatusForbidden}
+		w.WriteHeader(http.StatusForbidden)
+		return errForbidden
 	}
 	err = s.DeleteJob(vars["group"], vars["project"], vars["id"])
 	if err != nil {
-		return &apiErr{err.Error(), http.StatusInternalServerError}
+		w.WriteHeader(http.StatusInternalServerError)
+		return err
 	}
 	return nil
 }
 
-func createJob(a Authorizer, s Storage, w http.ResponseWriter, r *http.Request) error {
+func createJob(a authorizer, s storage, w http.ResponseWriter, r *http.Request) error {
 	var payload struct {
 		ID      string
 		Project string
@@ -134,10 +138,12 @@ func createJob(a Authorizer, s Storage, w http.ResponseWriter, r *http.Request) 
 	}
 	accessLevel, err := a.GetProjectAccessLevel(r, payload.Group, payload.Project)
 	if err != nil {
-		return &apiErr{err.Error(), http.StatusInternalServerError}
+		w.WriteHeader(http.StatusInternalServerError)
+		return err
 	}
 	if accessLevel != auth.ReadWrite {
-		return &apiErr{apiErrAccessForbidden.Error(), http.StatusForbidden}
+		w.WriteHeader(http.StatusForbidden)
+		return errForbidden
 	}
 	// TODO input validation
 	// TODO remove hardcoded values
@@ -165,38 +171,30 @@ func createJob(a Authorizer, s Storage, w http.ResponseWriter, r *http.Request) 
 	}
 	err = s.SaveJob(j)
 	if err != nil {
-		return &apiErr{err.Error(), http.StatusInternalServerError}
+		w.WriteHeader(http.StatusInternalServerError)
+		return err
 	}
 	return nil
 }
 
-func updateJob(_ Authorizer, s Storage, w http.ResponseWriter, r *http.Request) error {
+func updateJob(_ authorizer, s storage, w http.ResponseWriter, r *http.Request) error {
 	vars := mux.Vars(r)
 	log.Printf("Vars: %v\n", vars)
 	// TODO
 	return nil
 }
 
-type Storage interface {
-	GetJobs() ([]*model.Job, error)
-	GetGroupJobs(group string) ([]*model.Job, error)
-	GetProjectJobs(group string, project string) ([]*model.Job, error)
-	GetJob(group string, project string, id string) (*model.Job, error)
-	SaveJob(j *model.Job) error
-	DeleteJob(group string, project string, id string) error
-}
-
-func NewAPI(conf *conf.Conf, s Storage) {
+func NewAPI(conf *conf.Conf, s storage) {
 	r := mux.NewRouter()
 	v1 := r.PathPrefix("/v1").Subrouter()
 	auth := auth.GitLabAuthorizer{BaseURL: conf.GitLab.BaseURL}
-	v1.Handle("/jobs", &apiHandler{&auth, s, getJobs}).Methods("GET")
-	v1.Handle("/jobs", &apiHandler{&auth, s, createJob}).Methods("POST")
-	v1.Handle("/jobs/{group}", &apiHandler{&auth, s, getGroupJobs}).Methods("GET")
-	v1.Handle("/jobs/{group}/{project}", &apiHandler{&auth, s, getProjectJobs}).Methods("GET")
-	v1.Handle("/jobs/{group}/{project}/{id}", &apiHandler{&auth, s, getJob}).Methods("GET")
-	v1.Handle("/jobs/{group}/{project}/{id}", &apiHandler{&auth, s, deleteJob}).Methods("DELETE")
-	v1.Handle("/jobs/{group}/{project}/{id}", &apiHandler{&auth, s, updateJob}).Methods("PUT")
+	v1.Handle("/jobs", &handler{&auth, s, getJobs}).Methods("GET")
+	v1.Handle("/jobs", &handler{&auth, s, createJob}).Methods("POST")
+	v1.Handle("/jobs/{group}", &handler{&auth, s, getGroupJobs}).Methods("GET")
+	v1.Handle("/jobs/{group}/{project}", &handler{&auth, s, getProjectJobs}).Methods("GET")
+	v1.Handle("/jobs/{group}/{project}/{id}", &handler{&auth, s, getJob}).Methods("GET")
+	v1.Handle("/jobs/{group}/{project}/{id}", &handler{&auth, s, deleteJob}).Methods("DELETE")
+	v1.Handle("/jobs/{group}/{project}/{id}", &handler{&auth, s, updateJob}).Methods("PUT")
 	srv := &http.Server{
 		Handler: r,
 		Addr:    conf.API.Address,
