47b7a18b462cdce64f3de64a6c59716d506725f9
Michał Łowicki <mlowicki@gmail.com>
2018-09-15 22:39:33 +0200

    prefix secret's path with job's group, project and ID

--- mesos/mesos.go
+++ mesos/mesos.go
@@ -87,7 +87,8 @@ func newTaskInfo(j *model.Job, sec secrets) (error, *mesos.TaskInfo) {
 		env.Variables = append(env.Variables, mesos.Environment_Variable{Name: k, Value: func(v string) *string { return &v }(v)})
 	}
 	for k, v := range j.Secrets {
-		secret, err := sec.Read(v)
+		path := fmt.Sprintf("%s/%s/%s/%s", j.Group, j.Project, j.ID, v)
+		secret, err := sec.Read(path)
 		if err != nil {
 			return err, nil
 		}
